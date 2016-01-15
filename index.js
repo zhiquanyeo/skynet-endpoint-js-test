422b2a4c7f1a079a0436e05c9ec0b93bb007e47c
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2016-01-15 07:43:52 -0500

    Updated receiving code. Removed binaries/node_modules

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mqtt = require('mqtt');
+
+var client = mqtt.connect('tcp://localhost');
+
+var INPUT_REGEX = /skynet\/control\/([a-z]+)\/([0-9]+)$/;
 
 app.use('/', express.static(__dirname));
 
@@ -18,4 +23,25 @@ io.on('connection', function(socket) {
 
 http.listen(3000, function() {
 	console.log("Listening on *:3000");
+});
+
+client.on('connect', function () {
+	// Set up subscriptions
+	client.subscribe('skynet/control/#');
+	client.subscribe('skynet/clients/#');
+});
+
+client.on('message', function(topic, message) {
+	if (INPUT_REGEX.test(topic)) {
+		var matches = INPUT_REGEX.exec(topic);
+		var packet = {};
+		if (!packet[matches[1]]) packet[matches[1]] = {};
+		if (matches[1] === 'digital') {
+			packet[matches[1]][parseInt(matches[2], 10)] = message.toString() === '1' ? true : false;
+		}
+		else if (matches[1] === 'pwm') {
+			packet[matches[1]][parseInt(matches[2], 10)] = parseFloat(message.toString());
+		}
+		io.emit('robotUpdate', packet);
+	}
 });
